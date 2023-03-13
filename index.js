cf791b070c6b540b12b299c655da900dc6aa9ae1
ajsilesleiva <ajsilesleiva@gmail.com>
2023-03-13 16:15:05 -0600

    v 1.0

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // var spreadsheetID = "1oZ6z_l6OZ4BSu3qnlKZGj2_pD7L2it6D0Ki00swXkZs";
+    // var worksheetID = "COLOCA AQUÍ EL ID DE TU HOJA DE CÁLCULO";
+    var url = "https://docs.google.com/spreadsheets/d/1oZ6z_l6OZ4BSu3qnlKZGj2_pD7L2it6D0Ki00swXkZs/edit?usp=sharing";
+    
+    $.getJSON(url, function (data) {
+        var entry = data.feed.entry;
+
+        $(entry).each(function () {
+            $('.table').append('<tr><td>' + this.gsx$columna1.$t + '</td><td>' + this.gsx$columna2.$t + '</td><td>' + this.gsx$columna3.$t + '</td></tr>');
+        });
+    });
+});
