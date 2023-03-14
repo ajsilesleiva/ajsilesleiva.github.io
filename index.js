2e9bc3b4d9fbce4c9d7718f5c43dbffb304aaf95
ajsilesleiva <ajsilesleiva@gmail.com>
2023-03-13 21:21:02 -0600

    v 1.5

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-$(document).ready(function () {
-    // var spreadsheetID = "1oZ6z_l6OZ4BSu3qnlKZGj2_pD7L2it6D0Ki00swXkZs";
-    // var worksheetID = "COLOCA AQUÍ EL ID DE TU HOJA DE CÁLCULO";
-    var url = "https://docs.google.com/spreadsheets/d/1oZ6z_l6OZ4BSu3qnlKZGj2_pD7L2it6D0Ki00swXkZs/edit?usp=sharing";
-    
-    $.getJSON(url, function (data) {
-        var entry = data.feed.entry;
-        console.log(entry);
+document.addEventListener('DOMContentLoaded', function () {
+    const url = 'https://docs.google.com/spreadsheets/d/';
+    const ssid = '1ylE-7eTtdZo7PJsnVk-5ZklfyI1PHBY8vv7iJR5EFLA';
+    const q1 = '/gviz/tq?';
+    const q2 = 'tqx=out:json';
+    const q3 = 'sheet=Inventario 7_3_23';
+    let url1 = `${url}${ssid}${q1}&${q2}&${q3}`;
 
-        $(entry).each(function () {
-            $('.table').append('<tr><td>' + this.gsx$columna1.$t + '</td><td>' + this.gsx$columna2.$t + '</td><td>' + this.gsx$columna3.$t + '</td></tr>');
+    fetch(url1)
+        .then(response => response.text())
+        .then(data => {
+            var table = document.querySelector('.table');
+            var mainrow = document.querySelector('.mainrow');
+            var header = document.createElement('tr');
+            const json = JSON.parse(data.substr(47).slice(0, -2));
+            console.log(json.table);
+
+
+            json.table.rows.forEach((row) => {
+                console.log(col)
+                var col = document.createElement('div');
+                col.className = 'col';
+
+                var cardDiv = document.createElement('div');
+                cardDiv.className = 'card';
+
+                var cardImg = document.createElement('img');
+                cardImg.className = 'card-img-top';
+                cardImg.setAttribute('src', "https://cdn.shopify.com/s/files/1/0250/9661/8038/products/VITAL-C-HYDRATING-ANTI-AGING-SERUM-PDP-R01a_1400x.jpg?v=1644857333");
+                // cardImg.setAttribute('width', "100%");
+                cardImg.setAttribute('height', 100);
+              
+
+                var cardBody = document.createElement('div');
+                cardBody.className = 'card-body';
+
+                var cardTitle = document.createElement('h5');
+                cardTitle.className = 'card-title';
+                cardTitle.textContent = row.c[0]?.v;
+
+                var cardDesc = document.createElement('p');
+                cardDesc.className = 'card-text';
+                cardDesc.textContent = "Descripción: " + row.c[1]?.v ;
+             
+                var cardCant = document.createElement('p');
+                cardCant.className = 'card-text';
+                cardCant.textContent = "Stock: " + row.c[4]?.v ;
+
+                mainrow.appendChild(col);
+                col.appendChild(cardDiv);
+                cardBody.appendChild(cardImg);
+                cardDiv.appendChild(cardBody);
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(cardDesc);
+                cardBody.appendChild(cardCant);
+                // header.innerHTML = "<th>" + json.table.cols[0].label + "</th><th>" + json.table.cols[1].label + "</th><th>" + json.table.cols[4].label + "</th>";
+                // table.appendChild(header);
+            })
+            // json.table.rows.forEach((row) => {
+            //     // console.log(row.c);
+            //     var newRow = document.createElement('tr');
+            //     newRow.innerHTML = "<td>" + row.c[0]?.v + "</td><td>" + row.c[1]?.v + "</td><td>" + row.c[4]?.v + "</td>";
+            //     table.appendChild(newRow);
+            //     row.c.forEach((cell) => {
+            //         // console.log(cell)
+            //     })
+            // })
         });
-    });
-    // console.log($.getJSON());
 });
